44a3029c503b3f64e779e6b83698231f19c1173f
Mijanur Rahman <mahmudmizan717@gmail.com>
2025-06-25 14:47:25 +0600

    Remove build/ directory from tracking - now ignored by .gitignore

--- build/admin/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'react-jsx-runtime', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => 'ac152d44e888eca0846b');
